6719205a994cc0b17d5eb1c42807b300c0522989
nick-leslie <nick.leslie303@gmail.com>
2024-03-21 20:21:56 -0400

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div>
-      prototype one react starter
-    </div>
-  )
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            errorElement: <div />,
+            element: <Root />,
+            children: [
+                {
+                    path: "",
+                    element: <div>
+                        put routes here
+                    </div>
+                },
+            ],
+        },
+    ]);
+    return <RouterProvider router={router}></RouterProvider>
+    function Root() {
+        return (
+            <div className="w-full flex flex-col px-20 gap-5">
+                <h1>Welcome to your starter code.</h1>
+                <Outlet />
+            </div>
+        );
+    }
 }
 
 export default App
